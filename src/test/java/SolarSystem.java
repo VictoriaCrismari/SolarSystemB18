543c1cec3d0ae7f1dff69b56fde747847383d051
VictoriaCrismari <crismari.victoria1994@gmail.com>
2024-02-18 08:27:28 -0600

    commit

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -9,5 +9,10 @@ public class SolarSystem {
         xros.Color="pink";
         xros.temp="264545548k";
         xros.Size="15845841657461miles";
+
+        planet2 gecko =new planet2();
+        gecko.color="magenta";
+        gecko.temp="34343443434k";
+        gecko.size="342344342m";
     }
 }
